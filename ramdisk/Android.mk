c3acfccfd21c86d5cf8f9dc16ab7fd3568661edf
followmsi <followmsi@gmail.com>
2018-05-19 21:47:15 +0200

    gtaxl: twrp: add RECOVERY_SDCARD_ON_DATA

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -16,7 +16,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.samsungexynos7870
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := fstab.samsungexynos7870
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -24,7 +24,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.baseband.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := init.baseband.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -32,7 +32,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.rilcommon.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := init.rilcommon.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -40,7 +40,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.samsung.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := init.samsung.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -48,7 +48,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.samsungexynos7870.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := init.samsungexynos7870.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -56,7 +56,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.samsungexynos7870.usb.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := init.samsungexynos7870.usb.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -64,7 +64,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.wifi.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := init.wifi.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -72,7 +72,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.samsungexynos7870.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := ueventd.samsungexynos7870.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
