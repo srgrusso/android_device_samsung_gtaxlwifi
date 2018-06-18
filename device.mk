fbdb9583b5abc37f4f78ecb3695f761997f81bcf
followmsi <followmsi@gmail.com>
2018-06-18 19:46:07 +0200

    gtaxl: do not build AdvancedDisplay

--- device.mk
+++ device.mk
@@ -88,8 +88,8 @@ PRODUCT_PACKAGES += \
     libfimg
 
 # hardware/samsung/AdvancedDisplay (MDNIE)
-PRODUCT_PACKAGES += \
-    AdvancedDisplay
+#PRODUCT_PACKAGES += \
+#    AdvancedDisplay
 
 # Radio
 PRODUCT_PACKAGES += \
