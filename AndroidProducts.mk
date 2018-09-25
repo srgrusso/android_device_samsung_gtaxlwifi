thwalker3/android_device_samsung_gtanotexlwifi

gtaxlwifi: Consider lineage_gtaxlwifi product via the AOSP way

AndroidProducts.mk
@@ -1,0 +1,22 @@
+#
+# Copyright (C) 2015 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/lineage_gtaxlwifi.mk
+
+COMMON_LUNCH_CHOICES := \
+	lineage_gtaxl-userdebug \
+	lineage_gtaxl-eng

srgrusso/android_device_samsung_gtaxlwifi

AndroidProducts.mk
@@ -1,0 +1,22 @@
+#
+# Copyright (C) 2015 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/lineage_gtaxlwifi.mk
+
+COMMON_LUNCH_CHOICES := \
+	lineage_gtaxlwifi-userdebug \
+	lineage_gtaxlwifi-eng