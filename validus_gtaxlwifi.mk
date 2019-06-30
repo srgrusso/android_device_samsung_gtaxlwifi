4ab3b5143d85dc59b759a6df7a6fa2f4d042afa9
srgrusso <srgrusso240@gmail.com>
2019-06-29 19:35:07 -0500

    convert to validus

--- validus_gtaxlwifi.mk
+++ validus_gtaxlwifi.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/validus/config/common_full_tablet_wifionly.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_gtaxlwifi
