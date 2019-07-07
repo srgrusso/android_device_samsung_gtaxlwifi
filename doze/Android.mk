8fca2f97ece832fbf46960c6e16aa2fe900cbb51
srgrusso <srgrusso240@gmail.com>
2019-07-06 19:08:28 -0500

    Revert "gtaxl: SamsungDoze"
    This reverts commit 4ff5935f50465552dc6ce6b784783e66dba04d5e.

--- doze/Android.mk
+++ doze/Android.mk
@@ -7,33 +7,24 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := SamsungDoze
 LOCAL_CERTIFICATE := platform
-LOCAL_PRIVILEGED_MODULE := true
-
 LOCAL_PRIVATE_PLATFORM_APIS := true
+LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_USE_AAPT2 := true
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v4 \
     android-support-v13 \
-    android-support-v7-appcompat \
-    android-support-v7-preference \
     android-support-v7-recyclerview \
+    android-support-v7-preference \
+    android-support-v7-appcompat \
     android-support-v14-preference
-
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.lineageos.platform.internal
-
-LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
     $(TOP)/packages/resources/devicesettings/res
-		
-ifneq ($(INCREMENTAL_BUILDS),)
-    LOCAL_PROGUARD_ENABLED := disabled
-    LOCAL_JACK_ENABLED := incremental
-endif
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 include $(BUILD_PACKAGE)
 
