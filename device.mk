cbad81fa8be1f3774be924b0109b92c3607cb869
srgrusso <srgrusso240@gmail.com>
2019-10-30 20:38:09 -0500

    Remove Touch

--- device.mk
+++ device.mk
@@ -122,8 +122,8 @@ PRODUCT_PACKAGES += \
 #    SamsungDoze
 
 # Touch features
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.samsung
+#PRODUCT_PACKAGES += \
+#    vendor.lineage.touch@1.0-service.samsung
 
 # Camera
 PRODUCT_PACKAGES += \
