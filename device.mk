43177d051b1f2bba9bcb9ed972bb852cbbc66932
srgrusso <srgrusso240@gmail.com>
2019-12-26 20:03:40 -0600

    Remove SamsungDoze

--- device.mk
+++ device.mk
@@ -124,8 +124,8 @@ PRODUCT_PACKAGES += \
     android.hardware.cas@1.1-service
 
 # Doze
-PRODUCT_PACKAGES += \
-    SamsungDoze
+#PRODUCT_PACKAGES += \
+#    SamsungDoze
 
 # Touch features
 PRODUCT_PACKAGES += \
