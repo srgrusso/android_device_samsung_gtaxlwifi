d91ffd805c80a74f07c90f29a16ef35b8f59a8c0
srgrusso <srgrusso240@gmail.com>
2020-05-07 19:39:04 -0500

    Overide some more gapps

--- bliss_gtaxlwifi.mk
+++ bliss_gtaxlwifi.mk
@@ -31,7 +31,13 @@ $(call inherit-product, vendor/bliss/config/common_full_tablet_wifionly.mk)
 $(call inherit-product-if-exists, vendor/gapps/gapps.mk)
 
 TARGET_GAPPS_OVERRIDE += \
-    GoogleDialer
+    GoogleDialer \
+    GoogleCamera \
+    Photos \
+    PrebuiltGmail \
+    CalculatorGooglePrebuilt \
+    CalendarGooglePrebuilt \
+    NfcNci
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := bliss_gtaxlwifi
